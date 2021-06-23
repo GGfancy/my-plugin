a72036f8b8281f692f1f5878acf4b7d393dd315e
GitHub Action <action@github.com>
2021-06-23 07:19:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/ggfancy/myplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ggfancy/myplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ggfancy.myplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ggfancy.myplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ggfancy/myplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ggfancy/myplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ggfancy.myplugin.services
 
+import com.github.ggfancy.myplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
